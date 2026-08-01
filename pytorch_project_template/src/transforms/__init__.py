35924da7a73404bbbff68609632a6f2a0b9dc69d
chdpblks <tagtagchillboy@gmail.com>
2026-08-02 01:56:09 +0500

    commit

--- pytorch_project_template/src/transforms/__init__.py
+++ pytorch_project_template/src/transforms/__init__.py
@@ -1,6 +1,4 @@
 from src.transforms.normalize import Normalize1D
-from src.transforms.scale import RandomScale1D
-from src.transforms.stft import STFT
 from src.transforms.lfcc import LFCC
 from src.transforms.compose import Compose
 from src.transforms.trim_repeat import TrimRepeat
